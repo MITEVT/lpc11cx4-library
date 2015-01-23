a502d4ed79aa4fa5329cf9d2e675c813faef4b8e
ericponce <eaponce@mit.edu>
2015-01-23 02:02:36 -0500

    Added UART Example

---
+++ examples/uart/main.c
@@ -0,0 +1,69 @@
+#include "chip.h"
+
+const uint32_t OscRateIn = 12000000;
+
+#define LED_PIN 7
+
+volatile uint32_t msTicks;
+
+void SysTick_Handler(void) {
+	msTicks++;
+}
+
+__INLINE static void Delay(uint32_t dlyTicks) {
+	uint32_t curTicks = msTicks;
+	while ((msTicks - curTicks) < dlyTicks);
+}
+
+__INLINE static void GPIO_Config(void) {
+	Chip_GPIO_Init(LPC_GPIO);
+
+}
+
+__INLINE static void LED_Config(void) {
+	Chip_GPIO_WriteDirBit(LPC_GPIO, 0, LED_PIN, true);
+
+}
+
+__INLINE static void LED_On(void) {
+	Chip_GPIO_SetPinState(LPC_GPIO, 0, LED_PIN, true);
+}
+
+__INLINE static void LED_Off(void) {
+	Chip_GPIO_SetPinState(LPC_GPIO, 0, LED_PIN, false);
+}
+
+int main(void)
+{
+
+	SystemCoreClockUpdate();
+
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_6, (IOCON_FUNC1 | IOCON_MODE_INACT));/* RXD */
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_7, (IOCON_FUNC1 | IOCON_MODE_INACT));/* TXD */
+
+	Chip_UART_Init(LPC_USART);
+	Chip_UART_SetBaud(LPC_USART, 9600);
+	Chip_UART_ConfigData(LPC_USART, (UART_LCR_WLEN8 | UART_LCR_SBS_1BIT));
+	Chip_UART_SetupFIFOS(LPC_USART, (UART_FCR_FIFO_EN | UART_FCR_TRG_LEV2));
+	Chip_UART_TXEnable(LPC_USART);
+
+	const uint8_t *string = "Hello World\n\0";
+
+	if (SysTick_Config (SystemCoreClock / 1000)) {
+		//Error
+		while(1);
+	}
+
+	GPIO_Config();
+	LED_Config();
+
+	while(1) {
+		LED_On();
+		Chip_UART_SendBlocking(LPC_USART, string, 13);
+		Delay(1000);
+		LED_Off();
+		Delay(1000);
+	}
+
+	return 0;
+}
