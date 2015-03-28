30593397ebb8878ffbc46940cbea0bd9c0ac665b
ericponce <eaponce@mit.edu>
2015-03-28 16:27:13 +0200

    Now using evt_lib for itoa

--- examples/battery-monitor/src/main.c
+++ examples/battery-monitor/src/main.c
@@ -5,6 +5,7 @@
 
 
 #include "chip.h"
+#include "util.h"
 #include <string.h>
 #include <stdlib.h>
 
@@ -29,42 +30,6 @@ typedef struct Config Config_t;
 const uint32_t OscRateIn = 0000000;
 
 volatile uint32_t msTicks;
-
-int itoa(int value, char *sp, int radix) {
-    char tmp[16];// be careful with the length of the buffer
-    char *tp = tmp;
-    int i;
-    unsigned v;
-
-    int sign = (radix == 10 && value < 0);    
-    if (sign)
-        v = -value;
-    else
-        v = (unsigned) value;
-
-    while (v || tp == tmp) {
-        i = v % radix;
-        v /= radix; // v/=radix uses less CPU clocks than v=v/radix does
-        if (i < 10)
-          *tp++ = i+'0';
-        else
-          *tp++ = i + 'a' - 10;
-    }
-
-    int len = tp - tmp;
-
-    if (sign) {
-        *sp++ = '-';
-        len++;
-    }
-
-
-    while (tp > tmp) {
-        *sp++ = *--tp;
-	}
-
-    return len;
-}
 
 void SysTick_Handler(void) {
 	msTicks++;
