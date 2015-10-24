bfb3e478f8f5ead62c027a22e06010f78fc21701
Eric Ponce <eaponce@mit.edu>
2015-10-24 16:44:28 -0400

    fixed the fuxked can examples

--- examples/can/src/main.c
+++ examples/can/src/main.c
@@ -2,7 +2,7 @@
 
 #include "chip.h"
 #include "util.h"
-#include "string.h"
+#include <string.h>
 
 /*****************************************************************************
  * Private types/enumerations/variables
@@ -10,31 +10,34 @@
 
 #define TEST_CCAN_BAUD_RATE 500000
 
-#define LED_PORT 2
-#define LED_PIN 10
+#define LED_PORT 0
+#define LED_PIN 7
+
+#define BUFFER_SIZE 8
+#define UART_RX_BUFFER_SIZE 8
 
 const uint32_t OscRateIn = 12000000;
 
+
 volatile uint32_t msTicks;
 
+
+CCAN_MSG_OBJ_T msg_obj;
+STATIC RINGBUFF_T rx_buffer;
+CCAN_MSG_OBJ_T _rx_buffer[8];
+static bool can_error_flag;
+static bool can_error_info;
+
 static char str[100];
-
-static CCAN_MSG_OBJ_T msg_obj;
-static RINGBUFF_T rx_buffer;
-static CCAN_MSG_OBJ_T _rx_buffer[8];
-
-static bool can_error_flag = false;
-static uint32_t can_error_val = 0;
+static char uart_rx_buf[UART_RX_BUFFER_SIZE];
 
 #define DEBUG_ENABLE
 
 #ifdef DEBUG_ENABLE
 	#define DEBUG_Print(str) Chip_UART_SendBlocking(LPC_USART, str, strlen(str))
-	#define DEBUG_Println(str) {DEBUG_Print(str); DEBUG_Print("\r\n");}
 	#define DEBUG_Write(str, count) Chip_UART_SendBlocking(LPC_USART, str, count)
 #else
 	#define DEBUG_Print(str)
-	#define DEBUG_Println(str)
 	#define DEBUG_Write(str, count) 
 #endif
 
@@ -46,7 +49,8 @@ void SysTick_Handler(void) {
 	msTicks++;
 }
 
-void baudrateCalculate(uint32_t baud_rate, uint32_t *can_api_timing_cfg) {
+void baudrateCalculate(uint32_t baud_rate, uint32_t *can_api_timing_cfg)
+{
 	uint32_t pClk, div, quanta, segs, seg1, seg2, clk_per_bit, can_sjw;
 	Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_CAN);
 	pClk = Chip_Clock_GetMainClockRate();
@@ -75,6 +79,7 @@ void baudrateCalculate(uint32_t baud_rate, uint32_t *can_api_timing_cfg) {
 /*	Function is executed by the Callback handler after
     a CAN message has been received */
 void CAN_rx(uint8_t msg_obj_num) {
+	// LED_On();
 	/* Determine which CAN message has been received */
 	msg_obj.msgobj = msg_obj_num;
 	/* Now load up the msg_obj structure with the CAN message */
@@ -87,14 +92,16 @@ void CAN_rx(uint8_t msg_obj_num) {
 /*	CAN transmit callback */
 /*	Function is executed by the Callback handler after
     a CAN message has been transmitted */
-void CAN_tx(uint8_t msg_obj_num) {}
+void CAN_tx(uint8_t msg_obj_num) {
+	msg_obj_num = msg_obj_num;
+}
 
 /*	CAN error callback */
 /*	Function is executed by the Callback handler after
     an error has occurred on the CAN bus */
 void CAN_error(uint32_t error_info) {
+	can_error_info = error_info;
 	can_error_flag = true;
-	can_error_val = error_info;
 }
 
 /**
@@ -107,28 +114,15 @@ void CAN_IRQHandler(void) {
 	LPC_CCAN_API->isr();
 }
 
-uint8_t Rx_Buf[8];
+int main(void)
+{
 
-int main(void) {
-    // Set correct register settings to initialize the system clock 
 	SystemCoreClockUpdate();
-    uint8_t freq_tick_interrupts = SystemCoreClock / 1000;
-	if (SysTick_Config (freq_tick_interrupts)) {
-		// Error in system clock!
+
+	if (SysTick_Config (SystemCoreClock / 1000)) {
+		//Error
 		while(1);
 	}
-
-	Chip_TIMER_Init(LPC_TIMER32_0);
-	Chip_TIMER_Reset(LPC_TIMER32_0);
-	Chip_TIMER_MatchEnableInt(LPC_TIMER32_0, 0);
-	Chip_TIMER_SetMatch(LPC_TIMER32_0, 0, SystemCoreClock/2);
-	Chip_TIMER_ResetOnMatchEnable(LPC_TIMER32_0, 0);
-
-	Chip_TIMER_Enable(LPC_TIMER32_0);
-
-	/* Enable timer interrupt */
-	NVIC_ClearPendingIRQ(TIMER_32_0_IRQn);
-	NVIC_EnableIRQ(TIMER_32_0_IRQn);
 
 	//---------------
 	//UART
@@ -136,13 +130,13 @@ int main(void) {
 	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_7, (IOCON_FUNC1 | IOCON_MODE_INACT));/* TXD */
 
 	Chip_UART_Init(LPC_USART);
-	Chip_UART_SetBaud(LPC_USART, 57600);
+	Chip_UART_SetBaud(LPC_USART, 9600);
 	Chip_UART_ConfigData(LPC_USART, (UART_LCR_WLEN8 | UART_LCR_SBS_1BIT | UART_LCR_PARITY_DIS));
 	Chip_UART_SetupFIFOS(LPC_USART, (UART_FCR_FIFO_EN | UART_FCR_TRG_LEV2));
 	Chip_UART_TXEnable(LPC_USART);
 	//---------------
 
-	DEBUG_Print("Started up Receiver\n\r");
+	DEBUG_Print("Started up\n\r");
 
 	//---------------
 	//Ring Buffer
@@ -171,25 +165,57 @@ int main(void) {
 	LPC_CCAN_API->config_calb(&callbacks);
 	/* Enable the CAN Interrupt */
 	NVIC_EnableIRQ(CAN_IRQn);
-	
-	/* Configure message object 1 to receive all 11-bit messages */
+
+	// typedef struct CCAN_MSG_OBJ {
+	// 	uint32_t  mode_id;
+	// 	uint32_t  mask;
+	// 	uint8_t   data[8];
+	// 	uint8_t   dlc;
+	// 	uint8_t   msgobj;
+	// } CCAN_MSG_OBJ_T;
+
+	/* Configure message object 1 to only ID 0x600 */
 	msg_obj.msgobj = 1;
-	msg_obj.mode_id = 0x000;
-	msg_obj.mask = 0x000;
+	msg_obj.mode_id = 0x600;
+	msg_obj.mask = 0x7FF;
 	LPC_CCAN_API->config_rxmsgobj(&msg_obj);
 
-	while (1) {
-		if (can_error_flag) {
-			can_error_flag = false;
-			DEBUG_Print("Error: 0b");
-			itoa(can_error_val, str, 2);
-			DEBUG_Println(str);
-		}
+	
+	can_error_flag = false;
+	can_error_info = 0;
 
+	while (1) {
 		if (!RingBuffer_IsEmpty(&rx_buffer)) {
 			CCAN_MSG_OBJ_T temp_msg;
 			RingBuffer_Pop(&rx_buffer, &temp_msg);
-			DEBUG_Print("Received Message\n\r");
-        }
+			DEBUG_Print("Received Message ID: 0x");
+			itoa(temp_msg.mode_id, str, 16);
+			DEBUG_Print(str);
+			DEBUG_Print("\r\n");
+		}	
+
+		if (can_error_flag) {
+			can_error_flag = false;
+			DEBUG_Print("CAN Error: 0b");
+			itoa(can_error_info, str, 2);
+			DEBUG_Print(str);
+			DEBUG_Print("\r\n");
+		}
+
+		uint8_t count;
+		if ((count = Chip_UART_Read(LPC_USART, uart_rx_buf, UART_RX_BUFFER_SIZE)) != 0) {
+			switch (uart_rx_buf[0]) {
+				case 'a':
+					DEBUG_Print("Sending CAN with ID: 0x600\r\n");
+					msg_obj.msgobj = 2;
+					msg_obj.dlc = 1;
+					msg_obj.data[0] = 0xAA;
+					LPC_CCAN_API->can_transmit(&msg_obj);
+					break;
+				default:
+					DEBUG_Print("Invalid Command\r\n");
+					break;
+			}
+		}
 	}
 }
