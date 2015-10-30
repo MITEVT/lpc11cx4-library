455c31e32ce5b5d18dcdd3c67dc63dda54945e1a
Eric Ponce <eaponce@mit.edu>
2015-10-30 18:10:04 -0400

    Removed uneccesary utils

--- evt_lib/inc/util.h
+++ evt_lib/inc/util.h
@@ -7,7 +7,4 @@ void __reverse(char* begin,char* end);
 
 char* itoa(int value, char* buffer, int base);
 
-uint16_t getIDFromBytes(uint8_t high, uint8_t low);
-
-
 #endif
--- evt_lib/src/util.c
+++ evt_lib/src/util.c
@@ -11,38 +11,6 @@ void __reverse(char* begin,char* end) {
 	}
 }
 
-void uint8ToASCIIHex(uint8_t val, char* str) {
-	uint8_t high = val >> 4;
-	if (high < 10) {
-		str[0] = high | 0x30;
-	} else {
-		high -= 9;
-		str[0] = high | 0x40;
-	}
-
-	uint8_t low = val & 0x0F;
-	if (low < 10) {
-		str[1] = low | 0x30;
-	} else {
-		low -= 9;
-		str[1] = low | 0x40;
-	}
-}
-
-void uint16ToASCIIHex(uint16_t val, char* str) {
-	uint8ToASCIIHex(val >> 8, str);
-	uint8ToASCIIHex(val & 0x00FF, str + 2);
-}
-
-void uint32ToASCIIHex(uint32_t val, char* str) {
-	uint16ToASCIIHex(val >> 16, str);
-	uint16ToASCIIHex(val & 0xFFFF, str + 4);
-}
-
-uint16_t getIDFromBytes(uint8_t high, uint8_t low) {
-	return (low >> 5) | (high << 3);
-}
-
 char* itoa(int value, char* buffer, int base) {
 	static const char digits[] = "0123456789abcdef";
 
