95f9086b85642a4f194d69e10fb7523c2d9cb448
Nick Arango <narango@mit.edu>
2017-01-11 11:28:07 -0500

    added array size to util. removed itoa

--- evt_lib/inc/util.h
+++ evt_lib/inc/util.h
@@ -2,9 +2,8 @@
 #define _UTIL_H_
 
 #include "lpc_types.h"
+#define ARRAY_SIZE(foo) (sizeof(foo)/sizeof(foo[0]))
 
 void __reverse(char* begin,char* end);
-
-char* itoa(int value, char* buffer, int base);
 
 #endif
